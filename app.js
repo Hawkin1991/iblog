99efdef8e2ba9a676ce97f7a4ad08534bee99774
DESKTOP-4VV4PNC\Hawkin <jncz8888@outlook.com>
2017-01-05 13:35:27 +0800

    change jade to html

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var blog = require('./routes/blog');
+var album = require('./routes/album');
+var msg = require('./routes/msg');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +28,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/blog', blog);
+app.use('/album', album);
+app.use('/msg', msg);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
